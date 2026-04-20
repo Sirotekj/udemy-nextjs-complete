c6bb5b96577ed87361e221eab7c0201fe33d27e8
Sirotekj <sirotek.j@gmail.com>
2026-04-20 13:17:11 +0200

    First-app

---
+++ app/page.js
@@ -0,0 +1,16 @@
+import Link from "next/link";
+import Header from "@/components/header";
+export default function Home() {
+  return (
+    <main>
+      <Header />
+      <p>🔥 Let&apos;s get started! 🔥</p>
+      <p>
+        <Link href="/about">About Us</Link>
+      </p>
+      <p>
+        <Link href="/blog">Blog</Link>
+      </p>
+    </main>
+  );
+}
