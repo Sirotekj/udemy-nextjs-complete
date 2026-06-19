790d171a238363b9b471cfa8e7df16e8bfde37dd
Sirotekj <sirotek.j@gmail.com>
2026-06-19 10:04:48 +0200

    Authentication final

--- app/page.js
+++ app/page.js
@@ -1,38 +1,6 @@
-import { Suspense } from 'react';
+import AuthForm from '@/components/auth-form';
 
-import Posts from '@/components/posts';
-import { getPosts } from '@/lib/posts';
-
-const metadata = {
-  title: 'Latest Posts',
-  description: 'Browse and share amazing posts.',
-  openGraph: {
-    //for social networks
-    title: 'Latest Posts',
-    description: 'Browse and share amazing posts.',
-    url: 'next.js',
-    siteName: '',
-    images: [{ url: '', width: 800, height: 600 }],
-    locale: 'en_US',
-    type: 'website',
-  },
-};
-
-async function LatestPosts() {
-  const latestPosts = await getPosts(2);
-  return <Posts posts={latestPosts} />;
-}
-
-export default async function Home() {
-  return (
-    <>
-      <h1>Welcome back!</h1>
-      <p>Here's what you might've missed.</p>
-      <section id="latest-posts">
-        <Suspense fallback={<p>Loading recent posts...</p>}>
-          <LatestPosts />
-        </Suspense>
-      </section>
-    </>
-  );
+export default async function Home({ searchParams }) {
+  const formMode = searchParams.mode || 'login';
+  return <AuthForm mode={formMode} />;
 }
