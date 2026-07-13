50913ba9418c57a4bfb97c04e759bbd99406b862
Sirotekj <sirotek.j@gmail.com>
2026-07-13 14:33:17 +0200

    S13 final

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,44 @@
-import { getFeaturedEvents } from '../data/dummy-data';
-import EventList from '../components/events/event-list';
+import path from 'path';
+import fs from 'fs/promises';
 
-function HomePage() {
-  const featuredEvents = getFeaturedEvents();
+import Link from 'next/link';
+
+function HomePage(props) {
+  const { products } = props;
   return (
-    <div>
-      <EventList items={featuredEvents} />
-    </div>
+    <ul>
+      {products.map((product) => (
+        <li key={product.id}>
+          <Link href={`/products/${product.id}`}>{product.title}</Link>
+        </li>
+      ))}
+    </ul>
   );
 }
+
+export async function getStaticProps() {
+  console.log('(Re-)Generating...');
+  const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
+
+  if (!data) {
+    return {
+      redirect: {
+        destination: '/no-data', //example
+      },
+    };
+  }
+
+  if (data.products.length === 0) {
+    return { notFound: true };
+  }
+  return {
+    props: { products: data.products },
+    revalidate: 10,
+    //notFound: true,
+    //redirect: '/',
+  };
+}
+
 export default HomePage;
